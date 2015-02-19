63bf41a624fbc7a2a9eb95243a40c30b01963618
l0wran917 <laurent@bassin.info>
2015-02-19 15:46:21 +0100

    Ajout Projet

---
+++ desktop/src/fr/s2/projet/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.s2.projet.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.s2.projet.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
