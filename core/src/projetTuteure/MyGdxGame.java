6578be24a637e430cb8bddce52721d052c2a0a4b
Neonebula <Bryan.boukari@gmail.com>
2015-02-28 15:41:26 +0100

    Test

--- core/src/projetTuteure/MyGdxGame.java
+++ core/src/projetTuteure/MyGdxGame.java
@@ -13,7 +13,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("Thresh.jpg");
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
