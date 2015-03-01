9fcaa4575f725951e702aa1b89e813cc03572f62
Kraash91 <clement.mazeau@hotmail.fr>
2015-03-01 21:58:30 +0100

    test Map

---
+++ core/src/projetTuteure/Map.java
@@ -0,0 +1,42 @@
+package projetTuteure;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector;
+import com.badlogic.gdx.math.Vector2;
+
+public class Map {
+	private Vector2 tailleMap;
+	private Vector2 tailleTile;
+	private String nomImg;
+	
+	Map(String nom)
+	{
+		tailleMap = new Vector2();
+		tailleTile = new Vector2();
+		nomImg = new String();
+		
+		try {
+			Scanner fichier = new Scanner(Gdx.files.internal("../core/assets/"+nom).file());
+			tailleMap.x = fichier.nextInt();
+			tailleMap.y = fichier.nextInt();
+			tailleTile.x = fichier.nextInt();
+			tailleTile.y = fichier.nextInt();
+			nomImg = fichier.nextLine();
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}	
+	}
+	public static void main (String[] arg) {
+		Map map = new Map("map.txt");
+		
+		System.out.println(map.tailleMap.x);
+		System.out.println(map.tailleMap.y);
+		System.out.println(map.tailleTile.x);
+		System.out.println(map.tailleTile.y);
+		System.out.println(map.nomImg);
+	}
+}
