d6c9a8c2cff0214b3e71caef0bf71a07db7f0483
l0wran917 <laurent@bassin.info>
2015-03-01 22:29:02 +0100

    test

--- desktop/src/projetTuteure/desktop/DesktopLauncher.java
+++ desktop/src/projetTuteure/desktop/DesktopLauncher.java
@@ -1,24 +1,16 @@
 package projetTuteure.desktop;
 
-import com.badlogic.gdx.Audio;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.backends.lwjgl.audio.Mp3.Sound;
 import projetTuteure.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
-		config.width = 1920;
-		config.height = 1080;
-
-		config.fullscreen = true;
-		config.vSyncEnabled = true;
+		config.width = MyGdxGame.LARGEUR_ECRAN;
+		config.height = MyGdxGame.HAUTEUR_ECRAN;
 		
 		new LwjglApplication(new MyGdxGame(), config);
-		com.badlogic.gdx.audio.Sound sound = Gdx.audio.newSound(Gdx.files.internal("OMFG-Hello.mp3"));
-		sound.play(1.0f);
 	}
 }
