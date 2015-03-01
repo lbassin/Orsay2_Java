0eb09f32944806d8022cfa00e135c0fdbf1d8d8d
Kraash91 <clement.mazeau@hotmail.fr>
2015-03-01 23:15:19 +0100

    maj map

--- core/src/projetTuteure/Map.java
+++ core/src/projetTuteure/Map.java
@@ -2,40 +2,55 @@ package projetTuteure;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
 
 public class Map {
+	
 	private Vector2 tailleMap;
 	private Vector2 tailleTile;
 	private String nomImg;
+	private ArrayList<Tile> tiles;
+	private int nb;
+	private Texture img;
 	
 	Map(String nom)
 	{
 		tailleMap = new Vector2();
 		tailleTile = new Vector2();
 		nomImg = new String();
-		
+		tiles = new ArrayList<Tile>();
+		img = new Texture(nomImg);
 		Scanner fichier;
 		
-		try {
+		try 
+		{
 			fichier = new Scanner(new File("assets/"+nom));
-			
 			tailleMap.x = fichier.nextInt();
 			tailleMap.y = fichier.nextInt();
-			
 			tailleTile.x = fichier.nextInt();
 			tailleTile.y = fichier.nextInt();
-			
+			fichier.nextLine();
 			nomImg = fichier.nextLine();
+			nb = fichier.nextInt();
 			
-		} catch (FileNotFoundException e) {
+			int nbTileTexWidth = (int) (img.getWidth()/tailleTile.x);
+			
+			//tiles.add(new Tile();
+		} 
+		
+		catch (FileNotFoundException e) 
+		{
 			e.printStackTrace();
-		}
-	
+		}	
+		
 	}
+	
 	public static void main (String[] arg) {
 		Map map = new Map("map.txt");
 		
@@ -44,5 +59,6 @@ public class Map {
 		System.out.println(map.tailleTile.x);
 		System.out.println(map.tailleTile.y);
 		System.out.println(map.nomImg);
+		System.out.println(map.nb);
 	}
 }
