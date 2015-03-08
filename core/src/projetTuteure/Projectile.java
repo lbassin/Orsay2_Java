179cc7bcdd2f9e4dff076492dd4be0bfdecb47da
Neonebula <Bryan.boukari@gmail.com>
2015-03-08 23:00:40 +0100

    Ajout de la classe projectile et de l'image du projectile

---
+++ core/src/projetTuteure/Projectile.java
@@ -0,0 +1,49 @@
+package projetTuteure;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Projectile {
+    //Déclaration des variables de la classe
+    private Vector2 pos;
+    private Vector2 deplacement;
+
+    private Texture img;
+
+    private float vitesse;
+
+    //Constructeur de la classe
+    Projectile ()
+    {
+        pos = new Vector2();
+        deplacement = new Vector2();
+        img = new Texture("carre.jpg");
+        vitesse = 4;
+        pos.x = 1315;
+        this.pos.y = pos.y;
+    }
+
+    //Procèdure de mise à jour
+    public void update(Vector2 pos)
+    {
+        if (this.pos.x > 0)
+        {
+            deplacement.x = -vitesse;
+            System.out.println(pos.x + "En Y :" + pos.y);
+        }
+    }
+
+    //Procèdure de déplacement du projectile
+    public void deplacement()
+    {
+        pos.x += deplacement.x;
+        deplacement.x = 0;
+    }
+
+    //Procèdure
+    public void draw(SpriteBatch batch)
+    {
+        batch.draw(img, pos.x, pos.y);
+    }
+}
