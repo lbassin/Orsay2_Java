2072134e6696464a738d536fabcbb7613dbebca5
l0wran917 <laurent@bassin.info>
2015-04-15 09:10:19 +0200

    Suppr import inutile

--- editeurMap/core/src/editeurMap/Menu.java
+++ editeurMap/core/src/editeurMap/Menu.java
@@ -5,8 +5,6 @@ import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 public class Menu {
 	
