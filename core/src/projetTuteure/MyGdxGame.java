10bea83943a08a1bf480cbe5f91cbdeb4e0520ac
Neonebula <Bryan.boukari@gmail.com>
2015-04-29 21:52:59 +0200

    test bryann

--- core/src/projetTuteure/MyGdxGame.java
+++ core/src/projetTuteure/MyGdxGame.java
@@ -11,6 +11,8 @@ import com.badlogic.gdx.math.Vector2;
 
 public class MyGdxGame extends ApplicationAdapter {
 	
+	// Test
+	
 	// Constante
 	public static final int LARGEUR_ECRAN = 1312;
     public static final int HAUTEUR_ECRAN = 640;
