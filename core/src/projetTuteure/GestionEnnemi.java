ae4a3516d776fab24c6ec8033ec95fbb5673947b
Neonebula <Bryan.boukari@gmail.com>
2015-04-30 17:28:25 +0200

    Ajout de la classe GestionEnnemi qui gerera une ArrayList d'ennemis Ajout de toutes les opérations à faire pour tout les ennemis ici Ajout de l'ajout d'ennemis à une position définie dans un fichier externe, le nombre d'ennemi est aussi décidé dans ce fichier

---
+++ core/src/projetTuteure/GestionEnnemi.java
@@ -0,0 +1,65 @@
+package projetTuteure;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GestionEnnemi {
+	private ArrayList <Ennemi> ennemis;
+	private int nbEnnemis;
+	
+	GestionEnnemi(String nom)
+	{
+		ennemis = new ArrayList <Ennemi>();
+		Scanner fichier;
+		int posX, posY;
+		try
+		{
+			fichier = new Scanner (new File("../core/assets/"+nom));
+			nbEnnemis = fichier.nextInt();
+			for (int i=0; i<nbEnnemis; i++)
+			{
+				posX = fichier.nextInt();
+				posY = fichier.nextInt();
+				ennemis.add(new Ennemi(posX, posY));
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			System.err.println("Fichier d'ennemi non trouvé");
+			System.exit(5);
+		}
+	}
+	public ArrayList<Ennemi> getListeEnnemis()
+	{
+		return ennemis;
+	}
+	public int getNbEnnemis()
+	{
+		return nbEnnemis;
+	}
+	public void update(Perso cible)
+	{
+		for(int i=0; i<nbEnnemis; i++)
+			ennemis.get(i).update(cible);
+	}
+	public void deplacement ()
+	{
+		for(int i=0; i<nbEnnemis; i++)
+			ennemis.get(i).deplacement();
+	}
+	public void draw(SpriteBatch batch)
+	{
+		for(int i=0; i<nbEnnemis; i++)
+			ennemis.get(i).afficher(batch);
+	}
+	public void suppressionEnnemi()
+	{
+		for(int i=0; i<nbEnnemis; i++)
+			if (ennemis.get(i).getMort())
+				ennemis.remove(i);
+	}
+}
