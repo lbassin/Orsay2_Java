8c6d8524af247a4873bab83f23d96b141d431f27
Neonebula <Bryan.boukari@gmail.com>
2015-04-30 16:50:52 +0200

    Modification suite a la modification du nom de la procédure qui gère les projectiles du personnage

--- core/src/projetTuteure/MyGdxGame.java
+++ core/src/projetTuteure/MyGdxGame.java
@@ -100,7 +100,7 @@ public class MyGdxGame extends ApplicationAdapter {
 				ennemis.remove(i);
 		
 		// Supprime les projectiles sortie de l'ecran
-		perso.sortieEcranProjectile(camera);
+		perso.gestionProjectile(camera);
 		
 	}
 }
