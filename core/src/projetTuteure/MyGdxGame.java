66c59ca3a09a3cb67fe2c37a04b9c564f23fa41a
Laurent Bassin <laurent@bassin.info>
2015-06-11 18:44:58 +0200

    Suppresion import inutile || TODO : Collision ennemis / Tous les joueurs

--- core/src/projetTuteure/MyGdxGame.java
+++ core/src/projetTuteure/MyGdxGame.java
@@ -1,15 +1,11 @@
 package projetTuteure;
 
-import java.util.ArrayList;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 
 public class MyGdxGame extends ApplicationAdapter {
 	
